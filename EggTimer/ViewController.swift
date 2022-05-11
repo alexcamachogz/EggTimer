3eeabc625b1630964746edb90d9b9557bbea70d1
Alex Camacho <camacho.ale94@gmai.com>
2022-05-11 00:51:15 -0500

    feat: create a countdown timer and implement a message when the countdown is over

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,39 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
     
-
+    let eggTimes = [
+        "Soft": 3,
+        "Medium": 4,
+        "Hard": 7
+    ]
+    
+    var secondsRemaining = 60
+    var timer = Timer()
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        secondsRemaining = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if(secondsRemaining > 0) {
+            print(secondsRemaining)
+            secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            textLabel.text! = "Done"
+        }
+    }
+    
 
 }
